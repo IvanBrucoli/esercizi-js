dec0f7916fd5aec83fd856a126ecced596297903
Ivan Brucoli <ivan23197@gmail.com>
2024-01-15 17:18:00 +0100

    esercizio completato

---
+++ script.js
@@ -0,0 +1,49 @@
+//todo eserciccio 1
+
+
+
+// let uno = 1;
+// let due = 6;
+// let tre = 52;
+// let quattro = 45;
+// let cinque = 87;
+
+// let somma = uno+due+tre+quattro+cinque;
+
+// let media = somma/5;
+
+// console.log(`La somma tra i numeri equivale a ${somma} e la media risulta ${media}`);
+
+
+
+//todo eserciccio 2
+// let corrente = 2024;
+// let nascita = 1997;
+
+// let eta = corrente-nascita;
+// let vita = 100-eta;
+
+// console.log(`Hai ${eta} anni e ti mancano ${vita} anni per compierne 100`);
+
+
+
+//todo eserciccio 3
+
+
+// let gatti = 44;
+// let fila =6;
+// let file = parseInt(gatti/fila);
+// let resto = gatti%fila;
+// let mancanti = fila-resto;
+
+
+
+// console.log(`Ci sono ${file} file di gatti e ne mancano ${mancanti} per una nuova fila, con una avanzo di ${resto}`);
+
+
+
+
+
+
+
+
